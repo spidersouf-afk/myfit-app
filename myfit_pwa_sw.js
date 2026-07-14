e3a563fdc10ded28c5e904ac19e324a3542f4811
spidersouf-afk <spidersouf@gmail.com>
2026-07-14 15:08:09 +0100

    Add files via upload

---
+++ myfit_pwa_sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'myfit-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
